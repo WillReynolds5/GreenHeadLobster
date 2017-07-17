495380993017928005386e06405fc263b0a9441e
Scott Schibli <scott.schibli@reef-app.com>
2017-07-17 11:30:47 -0400

    scottscommit

--- login.js
+++ login.js
@@ -1,16 +1,46 @@
 
+// var fireRef = new firebase('https://green-head-lobster.firebaseio.com');
+// var database = firebase.database();
+(function(){
+
+
+  const loginButton = document.getElementById('loginButton')
+
+  // var fireRef = new firebase('https://green-head-lobster.firebaseio.com');
+  var database = firebase.database().ref();
+
+  loginButton.addEventListener('click', e => {
+    window.alert('clickstArted');
+    var email = document.getElementById("signUpEmail").value;
+    console.log(email);
+
+    var fullName = document.getElementById("name").value;
+    var password = document.getElementById("signUpPassword").value;
+
+
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (error != null) {
+          window.alert(errorMessage);
+          return;
+        }
+        console.log(fullName);
+        database.child(email).set(fullName);
+
+
+
+        // ...
+      });
+  })
+
+
+
+}());
+
+
 
 function signUp(){
 
-var email = document.getElementById("signUpEmail").value;
-console.log(email);
-
-var password = document.getElementById("signUpPassword").value;
-
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
 }
