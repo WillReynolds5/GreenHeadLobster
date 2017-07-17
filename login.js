3e0cfaa1730fbe35ac663863b2033210830e625f
WillReynolds5 <wmreynol@colby.edu>
2017-07-17 13:18:51 -0400

    will

--- login.js
+++ login.js
@@ -1,46 +1,60 @@
 
 // var fireRef = new firebase('https://green-head-lobster.firebaseio.com');
 // var database = firebase.database();
-(function(){
+(function() {
 
-
-  const loginButton = document.getElementById('loginButton')
-
+  const loginBtn = document.getElementById('loginBtn');
+  const signUpBtn = document.getElementById('signUpBtn');
   // var fireRef = new firebase('https://green-head-lobster.firebaseio.com');
   var database = firebase.database().ref();
 
-  loginButton.addEventListener('click', e => {
-    window.alert('clickstArted');
+  signUpBtn.addEventListener('click', e => {
+
+    // console.log(e.message);
     var email = document.getElementById("signUpEmail").value;
-    console.log(email);
 
     var fullName = document.getElementById("name").value;
     var password = document.getElementById("signUpPassword").value;
+    const signUp = firebase.auth().createUserWithEmailAndPassword(email, password);
 
+    signUp
+      .catch(e => console.log(e.message));
 
-      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        if (error != null) {
-          window.alert(errorMessage);
-          return;
+    const login = firebase.auth().signInWithEmailAndPassword(email, password);
+
+    login
+      .catch(e => console.log(e.message));
+
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+          database.child('users').child(firebaseUser.uid).set(fullName);
         }
-        console.log(fullName);
-        database.child(email).set(fullName);
-
-
-
-        // ...
       });
-  })
+
+  });
+
+  loginBtn.addEventListener('click', e => {
+    var email = document.getElementById("loginEmail").value;
+    var password = document.getElementById("loginPassword").value;
+
+    const login = firebase.auth().signInWithEmailAndPassword(email, password);
+
+    login
+      .catch(e => console.log(e.message));
+
+  });
+
+  firebase.auth().onAuthStateChanged(firebaseUser => {
+    if (firebaseUser) {
+      console.log(firebaseUser);
+      window.location.href = "";
+    }
+    else {
+      console.log('not logged in');
+    }
+
+  });
 
 
 
 }());
-
-
-
-function signUp(){
-
-}
