35c90b794b748a3726d541dae0fcac408be81e26
WillReynolds5 <wmreynol@colby.edu>
2017-07-20 15:38:06 -0400

    will

--- public/orderPage.js
+++ public/orderPage.js
@@ -18,12 +18,6 @@ function toOrderPage(x){
 
 }
 
-// window.onload = updateHeader(locationValue)
-
-// function updateHeader(y){
-//   console.log(y);
-//   document.getElementById("header").innerHTML = "stonington"
-// }
 
 function sunshine(){
 
@@ -34,12 +28,12 @@ function sunshine(){
     var Dict = JSON.parse(string);
     // console.log(Dict);
     for (var key in Dict){
-      var listing = Dict[key]
+      var listing = Dict[key];
       console.log(key);
-      var price = listing.Price
-      var inv = listing.Inventory
+      var price = listing.Price;
+      var inv = listing.Inventory;
 
-      myCreateFunction(key, inv, price)
+      myCreateFunction(key, inv, price);
     }
 
     // });
@@ -57,12 +51,12 @@ function stonington(){
     var Dict = JSON.parse(string);
     // console.log(Dict);
     for (var key in Dict){
-      var listing = Dict[key]
+      var listing = Dict[key];
       console.log(key);
-      var price = listing.Price
-      var inv = listing.Inventory
+      var price = listing.Price;
+      var inv = listing.Inventory;
 
-      myCreateFunction(key, inv, price)
+      myCreateFunction(key, inv, price);
     }
 
     // });
@@ -73,7 +67,77 @@ function stonington(){
 
 function webbsCove(){
 
-  var websInven = firebase.database().ref('WebsCove').orderByKey();
+  var websInven = firebase.database().ref('Webbs Cove').orderByKey();
+  websInven.on('value', function(snapshot) {
+    // snapshot.forEach(function(childSnapshot){
+    var string = JSON.stringify(snapshot);
+    var Dict = JSON.parse(string);
+    // console.log(Dict);
+    for (var key in Dict){
+      var listing = Dict[key];
+      console.log(key);
+      var price = listing.Price;
+      var inv = listing.Inventory;
+
+      myCreateFunction(key, inv, price);
+    }
+
+    // });
+
+  });
+
+}
+// admin
+
+function adminSunshine(){
+
+  var Inven = firebase.database().ref('Sunshine').orderByKey();
+  Inven.on('value', function(snapshot) {
+    // snapshot.forEach(function(childSnapshot){
+    var string = JSON.stringify(snapshot);
+    var Dict = JSON.parse(string);
+    // console.log(Dict);
+    for (var key in Dict){
+      var listing = Dict[key]
+      console.log(key);
+      var price = listing.Price;
+      var inv = listing.Inventory;
+
+      addExsistingListings(key, inv, price);
+    }
+
+    // });
+
+  });
+
+}
+function adminStonington(){
+
+
+  var Inven = firebase.database().ref('Stonington').orderByKey();
+  Inven.on('value', function(snapshot) {
+    // snapshot.forEach(function(childSnapshot){
+    var string = JSON.stringify(snapshot);
+    var Dict = JSON.parse(string);
+    // console.log(Dict);
+    for (var key in Dict){
+      var listing = Dict[key]
+      console.log(key);
+      var price = listing.Price;
+      var inv = listing.Inventory;
+
+      addExsistingListings(key, inv, price);
+    }
+
+    // });
+
+  });
+
+}
+
+function adminWebbsCove(){
+
+  var websInven = firebase.database().ref('Webbs Cove').orderByKey();
   websInven.on('value', function(snapshot) {
     // snapshot.forEach(function(childSnapshot){
     var string = JSON.stringify(snapshot);
@@ -82,10 +146,10 @@ function webbsCove(){
     for (var key in Dict){
       var listing = Dict[key]
       console.log(key);
-      var price = listing.Price
-      var inv = listing.Inventory
+      var price = listing.Price;
+      var inv = listing.Inventory;
 
-      myCreateFunction(key, inv, price)
+      addExsistingListings(key, inv, price);
     }
 
     // });
@@ -93,6 +157,8 @@ function webbsCove(){
   });
 
 }
+
+//creating rows
 
 function myCreateFunction(key, inv, price) {
     var table = document.getElementById("myTable");
@@ -107,6 +173,64 @@ function myCreateFunction(key, inv, price) {
     cell4.innerHTML = "<input type='text' value=''>";
 }
 
+function addNewListings() {
+    var table = document.getElementById("adminTable");
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = "<input class='baitType' type='text' value=''>";
+    cell2.innerHTML = "<input class='inv' type='text' value=''>";
+    cell3.innerHTML = "<input class='price' type='text' value=''>";
+    cell4.innerHTML = "<button class='deleteBtn'>Delete</button>";
+}
+
+function addExsistingListings(key, inv, price) {
+    var table = document.getElementById("adminTable");
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = "<input class='baitType' type='text' value='"+key+"'>";
+    cell2.innerHTML = "<input class='inv' type='text' value='"+inv+"'>";
+    cell3.innerHTML = "<input class='price' type='text' value='"+price+"'>";
+    cell4.innerHTML = "<button class='deleteBtn'>Delete</button>";
+}
+
+
 function myDeleteFunction() {
     document.getElementById("myTable").deleteRow(0);
+}
+
+//not pushing to the server properly... going as to many dictionaries
+
+
+function SaveAdminData(port) {
+  var adminTable = document.getElementById("adminTable");
+  var updateDict = [];
+  // console.log(adminTable.rows);
+  for (var i = 0; i < adminTable.rows.length - 1; i++) {
+    var rowDict = {};
+    var rowListing = {};
+
+          console.log("not header");
+          var bait = document.getElementsByClassName('baitType')[i].value;
+          console.log(bait);
+          console.log(i);
+          var inven = document.getElementsByClassName('inv')[i].value;
+          var price = document.getElementsByClassName('price')[i].value;
+          rowDict['Inventory'] = inven;
+          rowDict['Price'] = price;
+          rowListing[bait] = rowDict;
+          // console.log(port)
+          updateDict.push({
+            rowListing
+          });
+  }
+
+  console.log(updateDict);
+  console.log(port);
+  firebase.database().ref('port').set(updateDict);
 }
