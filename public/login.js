1715967834322cc53491c7df82b3b16de5a81c45
WillReynolds5 <wmreynol@colby.edu>
2017-07-21 19:18:30 -0400

    will

--- public/login.js
+++ public/login.js
@@ -1,75 +1,25 @@
 
-// var fireRef = new firebase('https://green-head-lobster.firebaseio.com');
-// var database = firebase.database();
-// (function() {
-  //
-  // const loginBtn = document.getElementById('loginBtn');
-  // const signUpBtn = document.getElementById('signUpBtn');
-  // var fireRef = new firebase('https://green-head-lobster.firebaseio.com');
-  // var database = firebase.database().ref();
-
-//  signUpBtn.addEventListener('click', e => {
-
-// function signUp(){
-//     console.log("signing Up");
-//     var email = document.getElementById("signUpEmail").value;
-//
-//     var fullName = document.getElementById("name").value;
-//     var password = document.getElementById("signUpPassword").value;
-//     const signUp = firebase.auth().createUserWithEmailAndPassword(email, password);
-//
-//     signUp
-//       .catch(e => console.log(e.message));
-//
-//     const login = firebase.auth().signInWithEmailAndPassword(email, password);
-//
-//     login
-//       .catch(e => console.log(e.message));
-//
-//     firebase.auth().onAuthStateChanged(firebaseUser => {
-//         if (firebaseUser) {
-//           database.child('users').child(firebaseUser.uid).set(fullName);
-//         }
-//       });
-//   }
-  //});
-
-
-  // loginBtn.addEventListener('click', e => {
-
-//   function login(){
-//     console.log("logining In");
-//     var email = document.getElementById("loginEmail").value;
-//     var password = document.getElementById("loginPassword").value;
-//
-//     const login = firebase.auth().signInWithEmailAndPassword(email, password);
-//
-//     login
-//       .catch(e => console.log(e.message));
-// }
-  // });
-
-  // firebase.auth().onAuthStateChanged(firebaseUser => {
-  //   if (firebaseUser) {
-  //     console.log(firebaseUser);
-  //     document.location.href = "homepage.html";
-  //   }
-  //   else {
-  //     console.log('not logged in');
-  //   }
-  //
-  // });
-
-// }());
 (function() {
-
-var database = firebase.database().ref();
 
     firebase.auth().onAuthStateChanged(firebaseUser => {
       if (firebaseUser) {
         console.log(firebaseUser);
         console.log("SWITCH");
-        document.location.href = "homepage.html";
+        var fullName = document.getElementById("name").value;
+        try {
+
+          var promise = firebase.database().ref('Users').child(firebaseUser.uid).set(fullName);
+            promise.then(function(){
+                document.location.href = "homepage.html";
+            });
+
+
+        } catch (e) {
+
+          console.log(e);
+
+        }
+
       }
       else {
         console.log('not logged in');
@@ -78,13 +28,13 @@ var database = firebase.database().ref();
     });
 }());
 
+
 function signUp(){
     console.log("signing Up");
     var email = document.getElementById("signUpEmail").value;
 
-    var fullName = document.getElementById("name").value;
+
     var password = document.getElementById("signUpPassword").value;
-    console.log(fullName);
     console.log(password);
     const signUp = firebase.auth().createUserWithEmailAndPassword(email, password);
 
@@ -93,11 +43,14 @@ function signUp(){
         console.log(e.message);
     });
 
-    firebase.auth().onAuthStateChanged(firebaseUser => {
-        if (firebaseUser) {
-          database.child('users').child(firebaseUser.uid).set(fullName);
-        }
-      });
+    // firebase.auth().onAuthStateChanged(firebaseUser => {
+    //     if (firebaseUser) {
+    //       alert('adding');
+    //       // firebase.database.ref('Users').child(firebaseUser.uid).set(fullName);
+    //     }else {
+    //       alert('fuck');
+    //     }
+    //   });
   }
 
 
--- public/orderPage.js
+++ public/orderPage.js
@@ -1,3 +1,15 @@
+//
+// $( document ).ready(function() {
+//
+// alert('');
+//     $('adminTable').on('click', 'button[type="button"]', function(e) {
+//       alert('');
+//     // var inputValue = $(this).closest("tr").find("input[type=text]").val();
+//     // var selectValuse = $(this).closest("tr").find("[name='select_job']").val();
+//     var index = $(this).closest("tr").remove();
+//   });
+// });
+
 
 
 function toOrderPage(x){
@@ -21,12 +33,11 @@ function toOrderPage(x){
 
 function sunshine(){
 
-  var Inven = firebase.database().ref('Sunshine').orderByKey();
+  var Inven = firebase.database().ref('Ports').child('Sunshine').orderByKey();
   Inven.on('value', function(snapshot) {
-    // snapshot.forEach(function(childSnapshot){
+
     var string = JSON.stringify(snapshot);
     var Dict = JSON.parse(string);
-    // console.log(Dict);
     for (var key in Dict){
       var listing = Dict[key];
       console.log(key);
@@ -36,7 +47,6 @@ function sunshine(){
       myCreateFunction(key, inv, price);
     }
 
-    // });
 
   });
 
@@ -44,7 +54,7 @@ function sunshine(){
 function stonington(){
 
 
-  var Inven = firebase.database().ref('Stonington').orderByKey();
+  var Inven = firebase.database().ref('Ports').child('Stonington').orderByKey();
   Inven.on('value', function(snapshot) {
     // snapshot.forEach(function(childSnapshot){
     var string = JSON.stringify(snapshot);
@@ -67,7 +77,7 @@ function stonington(){
 
 function webbsCove(){
 
-  var websInven = firebase.database().ref('Webbs Cove').orderByKey();
+  var websInven = firebase.database().ref('Ports').child('Webbs Cove').orderByKey();
   websInven.on('value', function(snapshot) {
     // snapshot.forEach(function(childSnapshot){
     var string = JSON.stringify(snapshot);
@@ -91,9 +101,8 @@ function webbsCove(){
 
 function adminSunshine(){
 
-  var Inven = firebase.database().ref('Sunshine').orderByKey();
+  var Inven = firebase.database().ref('Ports').child('Sunshine').orderByKey();
   Inven.on('value', function(snapshot) {
-    // snapshot.forEach(function(childSnapshot){
     var string = JSON.stringify(snapshot);
     var Dict = JSON.parse(string);
     // console.log(Dict);
@@ -106,7 +115,6 @@ function adminSunshine(){
       addExsistingListings(key, inv, price);
     }
 
-    // });
 
   });
 
@@ -114,9 +122,8 @@ function adminSunshine(){
 function adminStonington(){
 
 
-  var Inven = firebase.database().ref('Stonington').orderByKey();
+  var Inven = firebase.database().ref('Ports').child('Stonington').orderByKey();
   Inven.on('value', function(snapshot) {
-    // snapshot.forEach(function(childSnapshot){
     var string = JSON.stringify(snapshot);
     var Dict = JSON.parse(string);
     // console.log(Dict);
@@ -129,15 +136,13 @@ function adminStonington(){
       addExsistingListings(key, inv, price);
     }
 
-    // });
-
   });
 
 }
 
 function adminWebbsCove(){
 
-  var websInven = firebase.database().ref('Webbs Cove').orderByKey();
+  var websInven = firebase.database().ref('Ports').child('Webbs Cove').orderByKey();
   websInven.on('value', function(snapshot) {
     // snapshot.forEach(function(childSnapshot){
     var string = JSON.stringify(snapshot);
@@ -161,6 +166,7 @@ function adminWebbsCove(){
 //creating rows
 
 function myCreateFunction(key, inv, price) {
+  console.log('create!');
     var table = document.getElementById("myTable");
     var row = table.insertRow(1);
     var cell1 = row.insertCell(0);
@@ -183,8 +189,8 @@ function addNewListings() {
     cell1.innerHTML = "<input class='baitType' type='text' value=''>";
     cell2.innerHTML = "<input class='inv' type='text' value=''>";
     cell3.innerHTML = "<input class='price' type='text' value=''>";
-    cell4.innerHTML = "<button class='deleteBtn'>Delete</button>";
-}
+    cell4.innerHTML = "<button id='dBtn' class='deleteBtn' type='button' onClick='myDeleteFunction(this)'>Delete</button>";
+}//onClick='myDeleteFunction(this.row)'
 
 function addExsistingListings(key, inv, price) {
     var table = document.getElementById("adminTable");
@@ -196,41 +202,80 @@ function addExsistingListings(key, inv, price) {
     cell1.innerHTML = "<input class='baitType' type='text' value='"+key+"'>";
     cell2.innerHTML = "<input class='inv' type='text' value='"+inv+"'>";
     cell3.innerHTML = "<input class='price' type='text' value='"+price+"'>";
-    cell4.innerHTML = "<button class='deleteBtn'>Delete</button>";
+    cell4.innerHTML = "<button id='dBtn' class='deleteBtn' type=button' onClick='myDeleteFunction(this)'>Delete</button>";
+}//onClick='myDeleteFunction(this.row)'
+
+
+function myDeleteFunction(x) {
+    console.log('eeeeeee');
+    var table = document.getElementById("adminTable");
+    table.deleteRow(x.closest('tr').rowIndex);
+    console.log(x.closest('tr'));
+
 }
 
 
-function myDeleteFunction() {
-    document.getElementById("myTable").deleteRow(0);
-}
 
 //not pushing to the server properly... going as to many dictionaries
 
-
 function SaveAdminData(port) {
   var adminTable = document.getElementById("adminTable");
-  var updateDict = [];
+  var updateDict = {};
   // console.log(adminTable.rows);
   for (var i = 0; i < adminTable.rows.length - 1; i++) {
-    var rowDict = {};
-    var rowListing = {};
-
-          console.log("not header");
-          var bait = document.getElementsByClassName('baitType')[i].value;
-          console.log(bait);
-          console.log(i);
-          var inven = document.getElementsByClassName('inv')[i].value;
-          var price = document.getElementsByClassName('price')[i].value;
-          rowDict['Inventory'] = inven;
-          rowDict['Price'] = price;
-          rowListing[bait] = rowDict;
-          // console.log(port)
-          updateDict.push({
-            rowListing
-          });
+      var rowDict = {};
+      var bait = document.getElementsByClassName('baitType')[i].value;
+      var inven = document.getElementsByClassName('inv')[i].value;
+      var price = document.getElementsByClassName('price')[i].value;
+      rowDict['Inventory'] = inven;
+      rowDict['Price'] = price;
+      updateDict[bait] = rowDict;
   }
 
-  console.log(updateDict);
-  console.log(port);
-  firebase.database().ref('port').set(updateDict);
+try {
+  const update = firebase.database().ref('Ports').child(port).set(updateDict);
+  location.reload();
+} catch (err) {
+  alert('You cannot insert empty values or . , # , $ , / , [ , or ] in the textbox');
+}
+
+  // update.catch(err => {
+  //     console.log(err);
+  //     alert(err);
+  //   });
+
+
+
+
+}
+
+function createOrder(port) {
+  var myTable = document.getElementById("myTable");
+  var updateDict = {};
+  // console.log(adminTable.rows);
+  for (var i = 0; i < myTable.rows.length - 1; i++) {
+      var rowDict = {};
+      var bait = document.getElementsByClassName('baitType')[i].value;
+      var inven = document.getElementsByClassName('inv')[i].value;
+      var price = document.getElementsByClassName('price')[i].value;
+      rowDict['Inventory'] = inven;
+      rowDict['Price'] = price;
+      updateDict[bait] = rowDict;
+  }
+
+try {
+  const update = firebase.database().ref('Ports').child(port).set(updateDict);
+  location.reload();
+} catch (err) {
+  alert('You cannot insert empty values or . , # , $ , / , [ , or ] in the textbox');
+}
+
+  // update.catch(err => {
+  //     console.log(err);
+  //     alert(err);
+  //   });
+
+
+
+
 }
