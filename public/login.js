7b2e2033d8d868408a9807ac50da6bfc92587c3b
WillReynolds5 <wmreynol@colby.edu>
2017-07-25 10:04:12 -0400

    will

--- public/login.js
+++ public/login.js
@@ -1,47 +1,70 @@
 
 (function() {
 
-    firebase.auth().onAuthStateChanged(firebaseUser => {
-      if (firebaseUser) {
-        console.log(firebaseUser);
-        console.log("SWITCH");
-        var fullName = document.getElementById("name").value;
-        try {
-
-          var promise = firebase.database().ref('Users').child(firebaseUser.uid).set(fullName);
-            promise.then(function(){
-                document.location.href = "homepage.html";
-            });
-
-
-        } catch (e) {
-
-          console.log(e);
-
-        }
-
-      }
-      else {
-        console.log('not logged in');
-      }
-
-    });
+    // firebase.auth().onAuthStateChanged(firebaseUser => {
+    //   if (firebaseUser) {
+    //     console.log(firebaseUser);
+    //     console.log("SWITCH");
+    //     var fullName = document.getElementById("name").value;
+    //     try {
+    //
+    //       var promise = firebase.database().ref('Users').child(firebaseUser.uid).set(fullName);
+    //         promise.then(function(){
+    //             document.location.href = "homepage.html";
+    //         });
+    //
+    //
+    //     } catch (e) {
+    //
+    //       console.log(e);
+    //
+    //     }
+    //
+    //   }
+    //   else {
+    //     console.log('not logged in');
+    //   }
+    //
+    // });
 }());
 
 
 function signUp(){
     console.log("signing Up");
     var email = document.getElementById("signUpEmail").value;
-
-
     var password = document.getElementById("signUpPassword").value;
     console.log(password);
-    const signUp = firebase.auth().createUserWithEmailAndPassword(email, password);
+    var signUp = firebase.auth().createUserWithEmailAndPassword(email, password);
+
+    signUp.then(function(){
+      firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+          console.log(firebaseUser);
+          console.log("SWITCH");
+          var fullName = document.getElementById("name").value;
+          try {
+            var promise = firebase.database().ref('Users').child(firebaseUser.uid).set(fullName);
+              promise.then(function(){
+                console.log('everything done');
+                  checklogInFromIndex();
+              });
+          } catch (e) {
+            console.log(e);
+          }
+        }
+        else {
+          console.log('not logged in');
+        }
+      });
+    });
 
     signUp.catch(e => {
         alert(e.message);
         console.log(e.message);
     });
+
+
+
 
     // firebase.auth().onAuthStateChanged(firebaseUser => {
     //     if (firebaseUser) {
@@ -59,10 +82,24 @@ function signUp(){
     var email = document.getElementById("loginEmail").value;
     var password = document.getElementById("loginPassword").value;
 
-    const login = firebase.auth().signInWithEmailAndPassword(email, password);
+    var login = firebase.auth().signInWithEmailAndPassword(email, password);
+
+    login.then(function(){
+      checklogInFromIndex();
+    });
 
     login.catch(e => {
         alert(e.message);
         console.log(e.message);
     });
+}
+
+function checklogInFromIndex(){
+  firebase.auth().onAuthStateChanged(function(user){
+    console.log('runniong');
+    if (user){
+      document.location.href = "homepage.html";
+
+    }
+  });
 }
--- public/orderPage.js
+++ public/orderPage.js
@@ -1,16 +1,3 @@
-//
-// $( document ).ready(function() {
-//
-// alert('');
-//     $('adminTable').on('click', 'button[type="button"]', function(e) {
-//       alert('');
-//     // var inputValue = $(this).closest("tr").find("input[type=text]").val();
-//     // var selectValuse = $(this).closest("tr").find("[name='select_job']").val();
-//     var index = $(this).closest("tr").remove();
-//   });
-// });
-
-
 
 function toOrderPage(x){
   console.log(x);
@@ -174,9 +161,11 @@ function myCreateFunction(key, inv, price) {
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
     cell1.innerHTML = key;
+    cell1.setAttribute("class", "UserBaitType");
     cell2.innerHTML = inv;
     cell3.innerHTML = price;
-    cell4.innerHTML = "<input type='text' value=''>";
+    cell3.setAttribute("class", "UserPrice");
+    cell4.innerHTML = "<input type='text' class='UserQuantity' value=''>";
 }
 
 function addNewListings() {
@@ -207,7 +196,7 @@ function addExsistingListings(key, inv, price) {
 
 
 function myDeleteFunction(x) {
-    console.log('eeeeeee');
+
     var table = document.getElementById("adminTable");
     table.deleteRow(x.closest('tr').rowIndex);
     console.log(x.closest('tr'));
@@ -232,50 +221,141 @@ function SaveAdminData(port) {
       updateDict[bait] = rowDict;
   }
 
-try {
-  const update = firebase.database().ref('Ports').child(port).set(updateDict);
-  location.reload();
-} catch (err) {
-  alert('You cannot insert empty values or . , # , $ , / , [ , or ] in the textbox');
-}
-
-  // update.catch(err => {
-  //     console.log(err);
-  //     alert(err);
-  //   });
-
-
-
+  try {
+    const update = firebase.database().ref('Ports').child(port).set(updateDict);
+    location.reload();
+  } catch (err) {
+    alert('You cannot insert empty values or . , # , $ , / , [ , or ] in the textbox');
+  }
 
 }
 
-function createOrder(port) {
+
+function placeOrder(){
   var myTable = document.getElementById("myTable");
   var updateDict = {};
   // console.log(adminTable.rows);
+  var ports = localStorage.getItem("locationValue");
+  var name = '';
   for (var i = 0; i < myTable.rows.length - 1; i++) {
-      var rowDict = {};
-      var bait = document.getElementsByClassName('baitType')[i].value;
-      var inven = document.getElementsByClassName('inv')[i].value;
-      var price = document.getElementsByClassName('price')[i].value;
-      rowDict['Inventory'] = inven;
-      rowDict['Price'] = price;
-      updateDict[bait] = rowDict;
-  }
+    var qty = document.getElementsByClassName('UserQuantity')[i].value;
+      if (qty != ''){
+        // console.log(qty);
+        var rowDict = {};
+        var bait = document.getElementsByClassName('UserBaitType')[i].innerHTML;
+        console.log(bait);
+        var price = document.getElementsByClassName('UserPrice')[i].innerHTML;
+        rowDict['quantity'] = qty;
+        rowDict['Price'] = price * qty;
+        updateDict[bait] = rowDict;
+        // console.log(rowDict);
+      }
 
-try {
-  const update = firebase.database().ref('Ports').child(port).set(updateDict);
-  location.reload();
-} catch (err) {
-  alert('You cannot insert empty values or . , # , $ , / , [ , or ] in the textbox');
+  }
+  // console.log(updateDict);
+  firebase.auth().onAuthStateChanged(function(user){
+      name = user.uid;
+      console.log(name);
+      firebase.database().ref('Users').child(name).once('value').then(function(snapshot){
+
+          try {
+            console.log(ports);
+            console.log(snapshot.val());
+            console.log(updateDict);
+            const update = firebase.database().ref('Orders').child(ports).child(snapshot.val()).set(updateDict);
+            alert("Order Placed!");
+            location.reload();
+          } catch (err) {
+            console.log(err);
+            alert('You cannot insert empty values, letters or . , # , $ , / , [ , or ] in the textbox');
+          }
+      }, function(error) {
+        // The Promise was rejected.
+      console.error(error);
+      });
+    });
 }
 
-  // update.catch(err => {
-  //     console.log(err);
-  //     alert(err);
-  //   });
+function loadOrders(){
+
+  var tables = document.getElementById('orders');
+  var location = localStorage.getItem("locationValue");
+
+  firebase.database().ref('Orders').child(location).on('value', function(snapshot){
+
+
+    snapshot.forEach(function(child) {
+
+      var tab = document.createElement('table');
+
+      var header = tab.createTHead();
+      var confirmBtn = document.createElement('button');
+      // confirmBtn.type = 'button';
+      confirmBtn.setAttribute('class', 'AdminBtns');
+      header.innerHTML = child.key;
+      header.setAttribute('class', 'cptHeader');
+      tab.className = "tab";
+      var hRow = tab.insertRow(0);
+      var hcell1 = hRow.insertCell(0);
+      var hcell2 = hRow.insertCell(1);
+      var hcell3 = hRow.insertCell(2);
+      // var hcell4 = hRow.insertCell(3);
+      hcell1.innerHTML = "<h3 id='listingHeader'>Bait type</h3>";
+      hcell2.innerHTML = "<h3 id='listingHeader'>Quantity</h3>";
+      hcell3.innerHTML = "<h3 id='listingHeader'>Price</h3>";
+      // confirmBtn.setAttribute('id', 'adminBtn');
+
+      // hcell4.innerHTML = "<h3>delete</h3>";
+
+      var orderDict = {};
+      orderDict = child.val();
+      var string = JSON.stringify(orderDict);
+      var cptsOrder = JSON.parse(string);
+      // console.log(orderDict);
+      var cptName = child.key;
+      tab.setAttribute('id', cptName);
+      console.log(cptName);
+      // console.log(cptsOrder);
+
+      for (var x in cptsOrder){
+        console.log(x);
+        var listing = cptsOrder[x];
+        console.log(listing.Price);
+        console.log(listing.quantity);
+
+
+        var row = tab.insertRow(1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        // var cell4 = row.insertCell(3);
+        cell1.innerHTML = x;
+        cell2.innerHTML = "<input type='text' value='"+listing.quantity+"'>";
+        cell3.innerHTML = listing.Price;
+        // cell4.innerHTML = "<button id='dBtn' class='deleteBtn' type=button' onClick='cptOrderDeleteFunction(this)'>Delete</button>";
+
+
+        tables.appendChild(tab);
+
+        // document.getElementById('confirmButton').appendChild(confirmBtn);
 
 
 
+      }
+      var t = document.createTextNode('Confirm Order');
+      confirmBtn.appendChild(t);
+      tables.appendChild(confirmBtn);
+
+    });
+
+  });
+
+}
+
+function cptOrderDeleteFunction(x) {
+
+    var table = document.getElementById("adminTable");
+    table.deleteRow(x.closest('tr').rowIndex);
+    console.log(x.closest('tr'));
 
 }
